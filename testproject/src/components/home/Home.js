c206243dc1bd8a7339adecf6fd2adaa0725d4ef3
Ilya Aderiho <usc-mwnj@us.ibm.com>
2018-07-17 16:56:49 +0300

    add a Link to price on home page

--- testproject/src/components/home/Home.js
+++ testproject/src/components/home/Home.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import Info from './Info';
 import Service from './services/Service';
 
 const Home = () => <div>
+  <Link to='/price'>
+    Price
+  </ Link>
   <Info />
   <Service />
 </div>
