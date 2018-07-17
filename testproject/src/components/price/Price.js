9763aadd2ba0ffd5cc5bbf79dd91f7da10092e47
Ilya Aderiho <usc-mwnj@us.ibm.com>
2018-07-17 16:41:16 +0300

    add some lifecycles methodes, state declaration and changing, binding custom functions

--- testproject/src/components/price/Price.js
+++ testproject/src/components/price/Price.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Price = () => {
-  return <h1>Price component</h1>;
+class Price extends Component {
+
+    constructor(props){
+      super(props);
+      this.state = {value: 1};
+      this.increase = this.increase.bind(this);
+    }
+
+    componentWillMount(){ // will launch before rendering
+      console.log(`Will mount`);
+    }
+
+    componentDidMount(){ //will launch immediately after rendering
+      console.log(`Did mount`);
+      setTimeout(this.increase, 3000);
+    }
+
+    increase(){
+      this.setState(prevState => ({value: ++prevState.value}));
+    }
+
+    render () {
+      return <h1>Price component with value: {this.state.value}</h1>;
+    }
+
 }
 
 export default Price;
